886daf606223d86313f1c6ab3b9f054d13415e59
Omar Olmos <oolmos@ucmerced.edu>
2018-12-03 11:48:10 -0800

    Revert "Update"
    This reverts commit 0387388176d176f0d66ba4979cf4a7e4058c20b8.

--- src 11.45.24 AM/App.d
+++
@@ -1,2 +0,0 @@
-App.o: src/App.cpp src/../includes/App.h src/../includes/GlutApp.h \
-  src/../includes/Rect.h src/../includes/Shape.h
--- src 11.45.24 AM/Rect.d
+++
@@ -1,1 +0,0 @@
-Rect.o: src/Rect.cpp src/../includes/Rect.h src/../includes/Shape.h
